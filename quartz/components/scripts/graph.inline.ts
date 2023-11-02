4402a3f9e85384b2e7c522f74bc76ccb89f60419
Cittadhammo <82962905+cittadhammo@users.noreply.github.com>
2023-11-02 14:01:53 +0000

    add link in chart

--- quartz/components/scripts/graph.inline.ts
+++ quartz/components/scripts/graph.inline.ts
@@ -317,27 +317,7 @@ async function renderRadialGraph(container: string, fullSlug: FullSlug) {
 
   const data = await fetchData
 
-  // https://observablehq.com/@d3/radial-cluster/2?intent=fork
-
-  // Data for Radial Chart //
-  // look at https://d3js.org/d3-hierarchy/stratify#stratify_path for maybe a better way to extract data into tree
-
-  let dataRadial = []
-
-  for (const key in data) {
-    let crumb = key.split("/"),
-    crumbL = crumb.length,
-    parent = crumb[crumbL-2],
-    name = crumb[crumbL-1]
-    if(name=="index" && crumbL > 1) name = parent, parent = crumb[crumbL-3]
-    
-    if(parent != "tags") dataRadial.push({ id: name, parentId: parent == undefined ? (name=="index" ? "": "index") : parent, ...data[key]})
-  }
-  const dataStartified = d3.stratify()(dataRadial) // become a hierarchy usable in D3
-
-  // End Data for Radial Chart
-
-  // Dendrogram // 
+    // Dendrogram // 
 
   const height = Math.max(graph.offsetHeight, 250)
   const width = graph.offsetWidth
@@ -350,59 +330,178 @@ async function renderRadialGraph(container: string, fullSlug: FullSlug) {
    .attr("width", width)
    .attr("height", height)
    .attr("viewBox", [-width / 2 / scale, -height / 2 / scale, width / scale, height / scale])
-   .attr("style", "width: 100%; height: auto; font: 10px sans-serif;");
+   .attr("style", "width: 100%; height: auto; font: 16px sans-serif;");
   
+   let dataRadial = [];
+
+  for (const key in data) {
+    let crumb = key.split("/"),
+      crumbL = crumb.length,
+      parent = crumb[crumbL - 2],
+      name = crumb[crumbL - 1];
+    if (name == "index" && crumbL > 1)
+      (name = parent), (parent = crumb[crumbL - 3]);
+
+    if (parent != "tags")
+      dataRadial.push({
+        id: name,
+        parentId:
+          parent == undefined ? (name == "index" ? "" : "index") : parent,
+        ...data[key]
+      });
+  }
+  const dataStartified = d3.stratify()(dataRadial); // become a hierarchy usable in D3
+
+  // Dendrogram //
+
   const radius = Math.min(width, height) / 2 - 80;
 
   // Create a radial cluster layout. The layout’s first dimension (x)
   // is the angle, while the second (y) is the radius.
-  const tree = d3.cluster()
-      .size([2 * Math.PI, radius])
-      .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
+  const tree = d3
+    .cluster()
+    .size([2 * Math.PI, radius])
+    .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
 
   // Sort the tree and apply the layout.
   const root = tree(dataStartified);
-  
-  console.log(root)
+
   root.x = Math.PI / 2;
-  
+
+  const nodes = root.descendants();
+
+  let links = [];
+  nodes.forEach((node) => {
+    if (node.data.links) {
+      // exculde index page for the moment?
+      node.data.links.forEach((link) => {
+        if (nodes.find((n) => n.data.id == link)) {
+          links.push({
+            source: node,
+            target: nodes.find((n) => n.data.id == link)
+          });
+        }
+      });
+    }
+  });
+
+  let i = 10;
+
+  links.forEach((d) => {
+    const aS = d.source.x,
+      rS = d.source.y,
+      aT = d.target.x,
+      rT = d.target.y,
+      aM = (aS + aT) / 2, // middle angle
+      rM = (rS + rT) / 2; // middle radius
+    // sx = rS * Math.sin(aS), // cartesian coordinate
+    // sy = -rS * Math.cos(aS),
+    // tx = rS * Math.sin(aT),
+    // ty = -rS * Math.cos(aT),
+    // mx = rM * Math.sin(aM),
+    // my = -rM * Math.cos(aM);
+
+    // Draw the arc
+    if (rS == rT) {
+      i = i + 10;
+      svg
+        .append("path")
+        .attr(
+          "d",
+          d3
+            .arc()
+            .startAngle(aS)
+            .endAngle(aT)
+            .innerRadius(rS)
+            .outerRadius(rS + i)
+            .startAngle(aS)
+            .endAngle(aT)
+            .cornerRadius(10)
+        )
+        .attr("fill", "none")
+        .attr("stroke", "#555")
+        .attr("stroke-opacity", 0.4)
+        .attr("stroke-width", 1.5);
+      //.attr("transform", `translate(${mx}, ${my})`);
+
+      svg
+        .append("path")
+        .attr(
+          "d",
+          d3
+            .arc()
+            .startAngle(aS)
+            .endAngle(aT)
+            .innerRadius(rS)
+            .outerRadius(rS + 5)
+            .startAngle(aS)
+            .endAngle(aT)
+            .cornerRadius(10)
+        )
+        .attr("fill", "none")
+        .attr("stroke", "var(--light)")
+        .attr("stroke-opacity", 1)
+        .attr("stroke-width", 5);
+    }
+  });
   // Append links.
-  svg.append("g")
-      .attr("fill", "none")
-      .attr("stroke", "#555")
-      .attr("stroke-opacity", 0.4)
-      .attr("stroke-width", 1.5)
+  svg
+    .append("g")
+    .attr("fill", "none")
+    .attr("stroke", "#555")
+    .attr("stroke-opacity", 0.4)
+    .attr("stroke-width", 1.5)
     .selectAll()
     .data(root.links())
     .join("path")
-      .attr("d", d3.linkRadial()
-          .angle(d => d.x)
-          .radius(d => d.y));
+    .attr(
+      "d",
+      d3
+        .linkRadial()
+        .angle((d) => d.x)
+        .radius((d) => d.y)
+    );
 
   // Append nodes.
-  svg.append("g")
+  svg
+    .append("g")
     .selectAll()
     .data(root.descendants())
     .join("circle")
-      .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
-      .attr("fill", d => d.children ? "#555" : "#999")
-      .attr("r", 2.5);
-
+    .attr(
+      "transform",
+      (d) => `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0)`
+    )
+    .attr("fill", (d) => (d.children ? "#555" : "#999"))
+    .attr("r", 2.5);
   // Append labels.
-  svg.append("g")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-width", 3)
+  svg
+    .append("g")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-width", 3)
     .selectAll()
     .data(root.descendants())
     .join("text")
-      .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0) rotate(${d.x >= Math.PI ? 180 : 0})`)
-      .attr("dy", "0.31em")
-      .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
-      .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
-      .attr("paint-order", "stroke")
-      .attr("stroke", "white")
-      .attr("fill", "currentColor")
-      .text(d => d.data.title);
+    .attr(
+      "transform",
+      (d) =>
+        `rotate(${(d.x * 180) / Math.PI - 90}) translate(${d.y},0) rotate(${
+          d.x >= Math.PI ? 180 : 0
+        })`
+    )
+    .attr("dy", "0.31em")
+    .attr("x", (d) => (d.x < Math.PI === !d.children ? 6 : -6))
+    .attr("text-anchor", (d) =>
+      d.x < Math.PI === !d.children ? "start" : "end"
+    )
+    .attr("paint-order", "stroke")
+    .attr("stroke", "var(--light)")
+    .attr("fill", "currentColor")
+    .text((d) => d.data.title)
+    .clone(true)
+    .lower()
+    .attr("stroke", "var(--light)")
+    .attr("stroke-width", 5);
 
 }
 
